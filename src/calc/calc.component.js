47e1356c9619177be10ba72501bc0c7082f94e0c
Raynald Chung <raynaldchung@gmail.com>
2017-05-12 21:50:11 +0200

    Remove year 2015

--- src/calc/calc.component.js
+++ src/calc/calc.component.js
@@ -4,7 +4,7 @@ let calcComponent = {
   template,
   controller: function($scope, $location) {
     this.year = 2017;
-    if($location.search().year && [2015, 2016, 2017].indexOf(+$location.search().year) !== -1) {
+    if($location.search().year && [2016, 2017].indexOf(+$location.search().year) !== -1) {
       this.year = +$location.search().year;
     }
 
@@ -29,8 +29,6 @@ let calcComponent = {
     this.salaryOutputOptions = {
       'taxableYear': 'Taxable Income',
       'incomeTax': 'Income Tax',
-      'generalCredit': 'General Tax Credit',
-      'labourCredit': 'Labour Tax Credit',
       'netYear': 'Year net income',
       'netMonth': 'Monthly net income'
     };
@@ -47,18 +45,14 @@ let calcComponent = {
 
         let grossYear = this.salary.grossYear || 0;
         this.salary.taxableYear = grossYear;
-        // this.salary.generalCredit = getAlgemeneHeffingskorting(this.salary.taxableYear);
-        // this.salary.labourCredit = getArbeidskorting(this.salary.taxableYear);
         this.salary.grossMonth = ~~(grossYear / 12);
         this.salary.netYear = grossYear - getTaxAmount(this.salary.taxableYear, this.year);
-        // this.salary.netYear += this.salary.generalCredit + this.salary.labourCredit;
         this.salary.netMonth = ~~(this.salary.netYear / 12);
         this.salary.incomeTax = getTaxAmount(this.salary.taxableYear, this.year);
       });
 
     function getMinimumDeduction(year) {
       let amount = {
-          2015: 89050,
           2016: 91450,
           2017: 94750
       };
@@ -67,7 +61,6 @@ let calcComponent = {
 
     function getIncomeRate(year) {
       let rate = {
-          2015: 0.25,
           2016: 0.25,
           2017: 0.24
       };
@@ -76,15 +69,8 @@ let calcComponent = {
 
     function getTaxRates(ratesYear) {
       let taxRates = {
-        2015 : {
-          normal: [.365, .42, .42, .52],
-          withoutSocial: [.0835, .1385, .42, .52],
-          over64: [0.1860, 0.2410, .42, .52]
-        },
         2016 : {
-          normal: [.3655, .404, .404, .52],
-          withoutSocial: [.0835, .1385, .404, .52],
-          over64: [0.1860, 0.2250, .404, .52]
+          normal: [0, 0.0044, 0.017, 0.107, 0.137],
         },
         2017 : {
           normal: [0, 0.0093, 0.0241, 0.1152, 0.1452],
@@ -96,24 +82,19 @@ let calcComponent = {
 
     function getTaxAmountPeriods(year) {
       const taxAmountPeriods = {
-        2015:[
-          19822, // 0 - 19,822
-          13767, // 33,589 - 19,822
-          23996, // 57,585 - 33,589
-          Infinity
-        ],
         2016:[
-          19922, // 0 - 19,922
-          13793, // 33,715 - 19,922
-          32697, // 66,421 - 33,715
+          159800, // 159800 - 0
+          65100, // 224900 - 159800
+          340500, // 565400 - 224900
+          344100, // 909500 - 565400
           Infinity
         ],
         2017:[
           164100, // 164,100 - 0
-          66849, // 230,950 - 164,101
-          349699, // 580,650 - 230,951 
-          353399, // 934,050 - 580,651 
-          Infinity, 
+          66850, // 230,950 - 164,100
+          349700, // 580,650 - 230,950 
+          353400, // 934,050 - 580,650 
+          Infinity
         ],
       };
 
@@ -137,37 +118,6 @@ let calcComponent = {
       }
       return taxAmount;
     }
-
-    //labor discount
-    function getArbeidskorting(salary){
-      if(salary < 9147){
-        return salary * 1.793 / 100;
-      }
-      if(salary < 19758){
-        return 164 + (salary - 9147) * 27.698 / 100;
-      }
-      if(salary < 34015){
-        return 3103;
-      }
-      if(salary < 111590){
-        return 3103 - (salary - 34015) * 4 / 100;
-      }
-
-      return 0;
-    }
-
-    //general discount
-    function getAlgemeneHeffingskorting(salary) {
-      if(salary < 19922){
-        return 2242;
-      }
-      if(salary < 66417){
-        return 2242 - (salary - 19922) * 4.822 / 100;
-      }
-
-      return 0;
-    }
-
   }
 };
 
